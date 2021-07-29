012ddfcb5126382df4dd7e4adee187b82ce4ac39
Julio Muller <juliodolago@gmail.com>
2021-07-29 01:54:30 -0300

    Fix multi-env DB connection profiles

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,15 +6,15 @@ async function connect() {
   const options = await getConnectionOptions()
   const namingStrategy = new NamingStrategy()
   const logging = process.env.NODE_ENV === 'development'
-  const database = process.env.NODE_ENV === 'test'
-    ? 'tests/db.test.sqlite'
-    : options.database
+  const type = process.env.NODE_ENV === 'test' ? 'sqlite' : options.type
+  const database = process.env.NODE_ENV === 'test' ? ':memory:' : options.database
 
   return createConnection(
     Object.assign(options, {
       namingStrategy,
       database,
       logging,
+      type,
     }),
   )
 }
